e92530a07a0188f8c2503c3ba30d2afc1dec9250
Bobby Lin <mr.linjiahao@gmail.com>
2017-06-13 22:40:09 +0800

    Added WelcomeService (business layer) for refactoring.

---
+++ src/com/bobby/servlet/demo/WelcomeServletNew.java
@@ -0,0 +1,54 @@
+package com.bobby.servlet.demo;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bobby.service.demo.WelcomeService;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServletNew")
+public class WelcomeServletNew extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServletNew() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. Create data (model) and add it to request object
+		// Invoke business layer
+		WelcomeService welcomeService = new WelcomeService();
+		List<String> welcomeMessage = welcomeService.getWelcomeMessage("Albert");
+		request.setAttribute("myWelcomeMessage", welcomeMessage);
+		
+		// 2. Retrieve request dispatcher
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("welcomeNew.jsp");
+		
+		// 3. Forward request to view
+		requestDispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
